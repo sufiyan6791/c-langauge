14262b9c0c499f04142897b51983d35e49190ed7
sufiyan6791 <155509970+sufiyan6791@users.noreply.github.com>
2024-01-03 10:07:07 +0530

    lec1

---
+++ SUFIYAN.C
@@ -0,0 +1,40 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+main(){
+
+    clrscr();
+    printf("hey i m sufiyan");
+    printf("\n");
+    printf("\n *********    * * * * *    * * * * *   * *  *  *        ");
+    printf("\n *       *    *       *    *           *        *      ");
+    printf("\n *       *    * * * * *    *           *         *     ");
+    printf("\n * * * * *    *       *    *           *         *     ");
+    printf("\n *       *    *       *    *           *        *      ");
+    printf("\n *       *    * * * * *    * * * * *   * * * * *       ");
+    printf("\n");
+    printf("\n * * * * *    * * * * *    * * * * *    *       *     ");
+    printf("\n *            *            *            *       *     ");
+    printf("\n * * * *      * * * *      *            * * * * *       ");
+    printf("\n *            *            *   * * *    *       *      ");
+    printf("\n *            *            *   *   *    *       *         ");
+    printf("\n * * * * *    *            * * *   *    *       *        ");
+    printf("\n");
+    printf("\n  *******      *******     *    *        *                ");
+    printf("\n     *            *        *   *         *               ");
+    printf("\n     *            *        * *           *              ");
+    printf("\n     *            *        *  *          *              ");
+    printf("\n     *        *   *        *   *         *                  ");
+    printf("\n  *******     * * *        *     *       * * * * *            ");
+    printf("\n"      );
+    printf("\n  *       *   *       *   * * * * * *    * * * * *              ");
+    printf("\n  * *   * *   *  *    *   *         *    *       *           ");
+    printf("\n  *   *   *   *   *   *   *         *    * *  *  *            ");
+    printf("\n  *       *   *    *  *   *         *    *                  ");
+    printf("\n  *       *   *      **   *         *    *                ");
+    printf("\n  *       *   *       *   *  *  * * *    *                   ");
+
+
+
+}
