71cca6166d4cb9560fdfa2c5ab2ed2089e92f5cb
sufiyan6791 <155509970+sufiyan6791@users.noreply.github.com>
2024-01-03 12:39:23 +0530

    Add files via upload

---
+++ INTRODUC.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+ clrscr();
+
+ printf("name  :  sufiyan ansari");
+ printf("\n date of birth  :  24 october 2001");
+ printf(" \n qualification  :  graduate(b com) fullstack development  ");
+ printf("\n phone  :  6354924745");
+
+  getch();
+
+}
