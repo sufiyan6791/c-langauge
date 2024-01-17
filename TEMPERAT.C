06515ef0fe8ffd8b45556ef1887be0e0549a37cd
sufiyan6791 <155509970+sufiyan6791@users.noreply.github.com>
2024-01-17 19:28:31 +0530

    Add files via upload

---
+++ TEMPERAT.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+     float fahrenheit,celsius;
+
+      clrscr();
+
+      printf("enter value");
+      scanf("%f ",&celsius);
+     fahrenheit=((celsius*9)/5)+32;
+
+
+     printf("\n temperature in farhenheit is %f",fahrenheit );
+
+      getch();
+
+
+
+
+
+}
