911e822659fbfdb6ae48754859c99d7d52a13ca7
sufiyan6791 <155509970+sufiyan6791@users.noreply.github.com>
2024-01-17 19:25:38 +0530

    Add files via upload

---
+++ PATTERN.C
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+clrscr();
+
+    printf("\t\t\t*\t\t*");
+    printf("\n");
+    printf("\n\t\t*\t\t*");
+    printf("\n");
+    printf("\n\t*\t\t*");
+    printf("\n");
+    printf("\n\t\t*\t\t*");
+    printf("\n");
+    printf("\n");
+    printf("\t\t\t*\t\t*");
+    printf("\n - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ");
+    printf("\n");
+    printf("\n\t\t*\t*\t*\t*\t*\t*\t*");
+    printf("\n");
+    printf("\n");
+    printf("\n\t*\t*\t*\t*\t*\t*\t*\t*");
+    printf("\n");
+    printf("\n\t*\t\t*\t\t\t\t\t*");
+    printf("\n");
+    printf("\n\t*\t\t*\t\t*\t*\t*\t*");
+    printf("\n");
+    printf("\n\t*\t\t*\t\t*\t\t\t*");
+    printf("\n");
+    printf("\n\t*\t*\t*\t*\t*\t*\t*\t*");
+
+
+
+
+    getch();
+}
