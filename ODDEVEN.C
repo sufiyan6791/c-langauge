de5b8cc923e3fe36b5284d01d98cac9c129902c3
sufiyan6791 <155509970+sufiyan6791@users.noreply.github.com>
2024-02-01 18:19:14 +0530

    Add files via upload

---
+++ ODDEVEN.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int a;
+    clrscr();
+    printf("\n enter number:");
+    scanf("%d",&a);
+    (a%2==0)?printf("\n a is even no"):printf("\n a is odd no");
+
+
+
+
+  getch();
+}
