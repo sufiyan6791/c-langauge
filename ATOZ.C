b02b3ce8b7f5c4fac436eb23987b07df283b1ca0
sufiyan6791 <155509970+sufiyan6791@users.noreply.github.com>
2024-02-05 19:26:22 +0530

    Add files via upload

---
+++ ATOZ.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+  char ch='a';
+  clrscr();
+  printf("print a to z\n");
+  while(ch<='z') {
+  printf(" %c",ch);
+  ch++;
+  }
+   getch();
+
+
+
+
+}
